1985d159118259731dc762c0431fbb03b21b2814
Dmitry Marchenko <>
2025-06-20 12:54:14 +0300

    feat: init project

---
+++ Source/UIShowcase/UIShowcase.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "UIShowcase.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, UIShowcase, "UIShowcase" );
